0ab473e2ad7a2137741c23b8568ae8e785d4f034
amorscher <am.morscher@gmail.com>
2025-06-30 07:22:08 +0200

    migrate start and stop cmd

--- npm/src/cli.ts
+++ npm/src/cli.ts
@@ -1,17 +1,38 @@
-#!/usr/bin/env node
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
+import { PID_FILE, start } from "./cmds/start.js";
+import { stop } from "./cmds/stop.js";
 
 yargs(hideBin(process.argv))
-    .command("start", "start the cache server", (argv) => {
-        console.log(`👋  Starting server...`);
-    })
+    .command("start", "start the cache server",
+        y => y
+            .option('verbose', {
+                type: 'boolean',
+                alias: 'v',
+                describe: 'use verbose logging',
+                default: false,
+            })
+            .option('pidFile', {
+                type: 'string',
+                alias: 'p',
+                describe: 'where to creat the pid file',
+                default: PID_FILE,
+            }),
+        (args) => {
+            start(args.verbose,args.pidFile);
+        })
 
-    .command("stop", "stopping the cache server", (argv) => {
-        console.log(`👋  Stopping server...`);
-    })
+    .command("stop", "stopping the cache server", y => y
+        .option('pidFile', {
+            type: 'string',
+            alias: 'p',
+            describe: 'where to creat the pid file',
+            default: PID_FILE,
+        }), (argv) => {
+            stop(argv.pidFile);
+        })
 
-    .command("cache-stats", "stopping the cache server", (argv) => {
+    .command("cache-stats", "stopping the cache server", () => {
         console.log(`👋  Fetching cache stats...`);
     })
 
